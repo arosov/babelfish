9cb2991a9c20307294f337adf041ac1bd847f229
Alexis Rosovsky <arosov@github.io>
2026-02-08 23:29:15 +0100

    feat(server): disable connection idle timeout (set to 0)

--- src/babelfish_stt/server.py
+++ src/babelfish_stt/server.py
@@ -43,7 +43,7 @@ class BabelfishServer:
             config=ServerConfig(
                 certfile=self.server_config.cert_path,
                 keyfile=self.server_config.key_path,
-                connection_idle_timeout=10.0,
+                connection_idle_timeout=0.0,
                 keep_alive=True,
             )
         )
